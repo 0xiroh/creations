0befb98356d2bf75b7ba1d1cdd8475567a59f605
mgutierrezo <martin72.gutierrez@gmail.com>
2021-07-08 10:39:28 -0500

    rickandmortyapi1.2

--- scripts/rm.js
+++ scripts/rm.js
@@ -1,34 +1,41 @@
-apiUrl = "https://rickandmortyapi.com/api/character";
+
 const tarjeta = document.querySelector(".tarjeta");
 
-function usarApi(){
-    fetch(apiUrl)
+function usarApi(url){
+    fetch(url)
     .then(response => {
         return response.json();
     })
     .then(data => {
         console.log(data);
-        console.log(data.info.count);
+        console.log(data.results.length);
         const aleatorio = getRandomInt(data.results.length)
         renderizarTarjeta(data, aleatorio);
 
     })
 }
 
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
 function renderizarTarjeta(datos, n){
     const img = document.querySelector(".tarjeta img");
     const nombre = datos.results[n].name;
     const url = datos.results[n].image;
     tarjeta.innerHTML = `<img src="${url}"><h1>${nombre}</h1>`;
-
     
+    
+}
+
+function urlAleatoria(){
+    const aleatPage = getRandomInt(34)
+    apiUrl = "https://rickandmortyapi.com/api/character?page=" + aleatPage;
+    return apiUrl;
 }
 
 const btn = document.querySelector("#btn");
 btn.addEventListener("click", function() {
-    usarApi();
+    usarApi(urlAleatoria());
 });
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
